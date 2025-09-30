0ff99dda7afec9f93ae7496b82ed7ce99f4847ff
mansip13 <mansi.p@dataslush.com>
2025-09-30 11:45:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,5 @@ def delete_book(id):
     return {"deleted": deleted}, 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host ="0.0.0.0",port = port, debug = True)
